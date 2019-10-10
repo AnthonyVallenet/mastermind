2e68a6b80ae3504dac3da1527aeb3392ae3d0079
Anthony Vallenet <anthonyvallenet@MacBook-Pro-Anthony.local>
2019-10-10 15:24:04 +0200

    commit plateau

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.min.css';
 import Nav from './Nav.js';
+import Plateau from './Plateau.js';
 
 function App() {
   return (
     <div className="App">
       <Nav />
+      <main>
+        <Plateau />
+      </main>
     </div>
   );
 }
---
+++ src/Plateau.js
@@ -0,0 +1,239 @@
+import React from 'react';
+
+function Plateau() {
+  return (
+      <div className="Plateau">
+        <div className="line">
+            <div className="col">
+                <div className="case">
+                </div>
+                <div className="case">
+                </div>
+                <div className="case">
+                </div>
+                <div className="case">
+                </div>
+            </div>
+            <div className="col">
+                <div className="case">
+                </div>
+                <div className="case">
+                </div>
+                <div className="case">
+                </div>
+                <div className="case">
+                </div>
+            </div>
+        </div>
+
+        <div className="line">
+            <div className="col">
+                <div className="case">
+                </div>
+                <div className="case">
+                </div>
+                <div className="case">
+                </div>
+                <div className="case">
+                </div>
+            </div>
+            <div className="col">
+                <div className="case">
+                </div>
+                <div className="case">
+                </div>
+                <div className="case">
+                </div>
+                <div className="case">
+                </div>
+            </div>
+        </div>
+
+        <div className="line">
+            <div className="col">
+                <div className="case">
+                </div>
+                <div className="case">
+                </div>
+                <div className="case">
+                </div>
+                <div className="case">
+                </div>
+            </div>
+            <div className="col">
+                <div className="case">
+                </div>
+                <div className="case">
+                </div>
+                <div className="case">
+                </div>
+                <div className="case">
+                </div>
+            </div>
+        </div>
+
+        <div className="line">
+            <div className="col">
+                <div className="case">
+                </div>
+                <div className="case">
+                </div>
+                <div className="case">
+                </div>
+                <div className="case">
+                </div>
+            </div>
+            <div className="col">
+                <div className="case">
+                </div>
+                <div className="case">
+                </div>
+                <div className="case">
+                </div>
+                <div className="case">
+                </div>
+            </div>
+        </div>
+
+        <div className="line">
+            <div className="col">
+                <div className="case">
+                </div>
+                <div className="case">
+                </div>
+                <div className="case">
+                </div>
+                <div className="case">
+                </div>
+            </div>
+            <div className="col">
+                <div className="case">
+                </div>
+                <div className="case">
+                </div>
+                <div className="case">
+                </div>
+                <div className="case">
+                </div>
+            </div>
+        </div>
+
+        <div className="line">
+            <div className="col">
+                <div className="case">
+                </div>
+                <div className="case">
+                </div>
+                <div className="case">
+                </div>
+                <div className="case">
+                </div>
+            </div>
+            <div className="col">
+                <div className="case">
+                </div>
+                <div className="case">
+                </div>
+                <div className="case">
+                </div>
+                <div className="case">
+                </div>
+            </div>
+        </div>
+
+        <div className="line">
+            <div className="col">
+                <div className="case">
+                </div>
+                <div className="case">
+                </div>
+                <div className="case">
+                </div>
+                <div className="case">
+                </div>
+            </div>
+            <div className="col">
+                <div className="case">
+                </div>
+                <div className="case">
+                </div>
+                <div className="case">
+                </div>
+                <div className="case">
+                </div>
+            </div>
+        </div>
+
+        <div className="line">
+            <div className="col">
+                <div className="case">
+                </div>
+                <div className="case">
+                </div>
+                <div className="case">
+                </div>
+                <div className="case">
+                </div>
+            </div>
+            <div className="col">
+                <div className="case">
+                </div>
+                <div className="case">
+                </div>
+                <div className="case">
+                </div>
+                <div className="case">
+                </div>
+            </div>
+        </div>
+
+        <div className="line">
+            <div className="col">
+                <div className="case">
+                </div>
+                <div className="case">
+                </div>
+                <div className="case">
+                </div>
+                <div className="case">
+                </div>
+            </div>
+            <div className="col">
+                <div className="case">
+                </div>
+                <div className="case">
+                </div>
+                <div className="case">
+                </div>
+                <div className="case">
+                </div>
+            </div>
+        </div>
+
+        <div className="line">
+            <div className="col">
+                <div className="case">
+                </div>
+                <div className="case">
+                </div>
+                <div className="case">
+                </div>
+                <div className="case">
+                </div>
+            </div>
+            <div className="col">
+                <div className="case">
+                </div>
+                <div className="case">
+                </div>
+                <div className="case">
+                </div>
+                <div className="case">
+                </div>
+            </div>
+        </div>
+      </div>
+  );
+}
+
+export default Plateau;
