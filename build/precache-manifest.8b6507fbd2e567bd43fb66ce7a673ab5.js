7799df6deb626bdf188d6b9244e7e7f1641016e6
Anthony Vallenet <anthonyvallenet@MacBook-Pro-Anthony.local>
2019-10-22 17:26:42 +0200

    Create a React app and publish it to GitHub Pages

---
+++ build/precache-manifest.8b6507fbd2e567bd43fb66ce7a673ab5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b49296465b0ac6f8f5d94d4c57767a17",
+    "url": "/mastermind/index.html"
+  },
+  {
+    "revision": "cf684208cd8069d56bf8",
+    "url": "/mastermind/static/css/main.41e09fe0.chunk.css"
+  },
+  {
+    "revision": "d34d48e177e4b9c3ccf9",
+    "url": "/mastermind/static/js/2.c6200b45.chunk.js"
+  },
+  {
+    "revision": "cf684208cd8069d56bf8",
+    "url": "/mastermind/static/js/main.009e1ad2.chunk.js"
+  },
+  {
+    "revision": "32639ebdd51befc465c9",
+    "url": "/mastermind/static/js/runtime-main.0a89982d.js"
+  }
+]);
