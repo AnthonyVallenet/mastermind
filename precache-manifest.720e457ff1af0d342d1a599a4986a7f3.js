12ae2320ccb4cae160420238af02b812915bdaef
Anthony Vallenet <anthonyvallenet@MacBook-Pro-Anthony.local>
2019-10-23 14:32:34 +0200

    Updates

---
+++ precache-manifest.720e457ff1af0d342d1a599a4986a7f3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "065c7c756a2cad57a1fee27e3a952aac",
+    "url": "/mastermind/index.html"
+  },
+  {
+    "revision": "7137784d654661765d3b",
+    "url": "/mastermind/static/css/main.41e09fe0.chunk.css"
+  },
+  {
+    "revision": "44b9fd1d26b1e6278eed",
+    "url": "/mastermind/static/js/2.42fb0696.chunk.js"
+  },
+  {
+    "revision": "7137784d654661765d3b",
+    "url": "/mastermind/static/js/main.1f26b14e.chunk.js"
+  },
+  {
+    "revision": "32639ebdd51befc465c9",
+    "url": "/mastermind/static/js/runtime-main.0a89982d.js"
+  }
+]);
